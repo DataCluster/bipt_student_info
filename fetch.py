ed1168602628d4efd0dab8f687d77da131d502df
王万霖 <dgideas@outlook.com>
2015-10-02 20:40:18 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,3 +1,8 @@
 __author__=DGideas;
 
 import pyfetch;
+import DGStorage as DGS;
+i=100;
+while i<999:
+	pyfetch.fetch("http://xsfw.bipt.edu.cn/fwzx/portal/xxcx/cx_jbxx_gr.jsp?yonghm=5120150"+str(i),"stuinfo.htm");
+	i+=1;
